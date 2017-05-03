7c164b7916ff1ddf595a1361865719b435165bae
Zainal Fahrudin <fnzainal@gmail.com>
2017-05-03 21:41:31 +0700

    update frame name

--- src/com/fnzainal/AppO.java
+++ src/com/fnzainal/AppO.java
@@ -19,7 +19,7 @@ public class AppO {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("AppO");
+        JFrame frame = new JFrame("App O");
         frame.setContentPane(new AppO().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
